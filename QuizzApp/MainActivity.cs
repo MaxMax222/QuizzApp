311ba0194a445215c1173fdd9ae20dd234697be3
Maxim Zyuryaev <zrv.maxim@gmail.com>
2024-09-24 08:36:45 +0300

    Improved UI

--- QuizzApp/MainActivity.cs
+++ QuizzApp/MainActivity.cs
@@ -2,19 +2,45 @@
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using Android.Widget;
+using System;
+using Android.Content;
 
 namespace QuizzApp
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Button next;
+        TextView score; 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            Init();
+            AddClicks();
         }
+
+        private void AddClicks()
+        {
+            next.Click += Next_Click;
+        }
+
+        private void Next_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(Questions));
+            StartActivity(intent);
+        }
+
+        void Init()
+        {
+            next = FindViewById<Button>(Resource.Id.next);
+            score = FindViewById<TextView>(Resource.Id.score);
+        }
+
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
