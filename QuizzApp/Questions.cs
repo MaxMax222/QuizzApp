c6698e04a35332078d8bd5b1571dfd5ca704a883
Maxim Zyuryaev <zrv.maxim@gmail.com>
2024-09-26 16:36:39 +0300

    fixed amount of questions and score calculation

--- QuizzApp/Questions.cs
+++ QuizzApp/Questions.cs
@@ -20,7 +20,7 @@ namespace QuizzApp
         Random rnd = new Random();
         ImageView[] images;
         int[] radioButtonIds;
-        int currentQuestion, score = 0, amount_of_questions = 10;
+        int currentQuestion, score = 0, amount_of_questions = 9;
         string msg = "";
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -40,37 +40,43 @@ namespace QuizzApp
 
         private void Next_Click(object sender, EventArgs e)
         {
-            if (currentQuestion < amount_of_questions - 1)
+            if (currentQuestion < amount_of_questions)
             {
-                // Check if the selected answer is correct
-                if (options.CheckedRadioButtonId == radioButtonIds[int.Parse(images[currentQuestion].Tag.ToString())])
-                {
-                    score++;
-                    msg = "Correct Answer";
-                }
-                else
-                {
-                    msg = "Wrong Answer";
-                }
+                CheckAnswer();
                 currentQuestion++;
                 GenerateQuestion();
                 Toast.MakeText(this, msg, ToastLength.Short).Show();
             }
             else
             {
+                CheckAnswer();
                 // End of quiz, go to main activity
                 var intent = new Intent(this, typeof(MainActivity));
                 intent.PutExtra("score", score.ToString());
                 StartActivity(intent);
             }
-        }
 
+
+        }
+        void CheckAnswer()
+        {
+            // Check if the selected answer is correct
+            if (options.CheckedRadioButtonId == radioButtonIds[int.Parse(images[currentQuestion - 1].Tag.ToString())])
+            {
+                score++;
+                msg = "Correct Answer";
+            }
+            else
+            {
+                msg = "Wrong Answer";
+            }
+        }
         private void Init()
         {
             options = FindViewById<RadioGroup>(Resource.Id.select_options);
             next = FindViewById<Button>(Resource.Id.next);
             container = FindViewById<LinearLayout>(Resource.Id.imageContainer);
-            currentQuestion = 0;
+            currentQuestion = 1;
 
             images = GenerateImages();
             GenerateQuestion();
@@ -99,7 +105,7 @@ namespace QuizzApp
             next.Enabled = false; // Disable next button until an option is selected
             options.ClearCheck(); // Clear any previously selected option
             container.RemoveAllViews(); // Clear the previous image
-            container.AddView(images[currentQuestion]); // Add the current image
+            container.AddView(images[currentQuestion - 1]); // Add the current image
         }
 
         ImageView[] GenerateImages()
